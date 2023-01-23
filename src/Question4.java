f4b215fd470312277fa559760bce3ccf2db5bab2
AlakabakSumeyye <you@example.com>
2023-01-23 20:35:12 +0400

    23 Januaryy

--- src/Question4.java
+++ src/Question4.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Question4 {
+import java.util.Scanner;
+
+public class Question4 {
+    public static void main(String[] args) {
+        /*
+        Get a three-digit number from the user.
+        Print the sum of the digits of this number received from the user on the screen.
+         */
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Please enter a three digit number");
+        int number=scan.nextInt();
+        int onesDigit=0;
+        int sum=0;
+
+        onesDigit=number%10;
+        sum+=onesDigit;
+        number/=10;
+
+        onesDigit=number%10;
+        sum+=onesDigit;
+        number/=10;
+
+        onesDigit=number%10;
+        sum+=onesDigit;
+        number/=10;
+
+        System.out.println("The sum of the digits of the number you entered: " + sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
--- src/Question5.java
+++ src/Question5.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Question5 {
+import java.util.Scanner;
+
+public class Question5 {
+    public static void main(String[] args) {
+        /*
+        Ask the user for their weight and height
+        and write a program that calculates their body mass index.
+         */
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Please enter your weight in kg.");
+        double weight= scan.nextDouble();
+
+        System.out.println("Please enter your height in cm.");
+        double height= scan.nextDouble();
+
+        double bmi= weight/(height * height);
+
+
+        if (bmi>=30)
+            System.out.println("You are obese, you should take care of yourself before it's too late");
+
+        else if (bmi>=25)
+            System.out.println("You are overweight, you should start the diet");
+
+        else if (bmi>=20)
+            System.out.println("Your weight is normal, continue with it");
+
+        else System.out.println("You are thin, you should gain weight");
+
+        System.out.println("Your body mass index : " + bmi);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
